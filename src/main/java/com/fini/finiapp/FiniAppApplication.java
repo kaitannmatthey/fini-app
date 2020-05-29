2dad141812e62f733010a7c3979ff67a9f990e62
Kait Ann Matthey <kaitannmatthey@gmail.com>
2020-05-29 18:39:53 -0400

    init commit

---
+++ src/main/java/com/fini/finiapp/FiniAppApplication.java
@@ -0,0 +1,13 @@
+package com.fini.finiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiniAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fini/finiapp/FiniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fini.finiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
